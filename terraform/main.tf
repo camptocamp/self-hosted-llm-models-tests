3627ac4586a917f05847860788d08137a300fc0b
Gonçalo Heleno <goncalo.heleno@camptocamp.com>
2024-08-21 15:15:48 +0200

    fix: revert to last working version of Longhorn

--- terraform/main.tf
+++ terraform/main.tf
@@ -120,7 +120,7 @@ module "cert-manager" {
 }
 
 module "longhorn" {
-  source = "git::https://github.com/camptocamp/devops-stack-module-longhorn.git?ref=v3.7.0"
+  source = "git::https://github.com/camptocamp/devops-stack-module-longhorn.git?ref=v3.6.0"
 
   cluster_name   = module.sks.cluster_name
   base_domain    = module.sks.base_domain
