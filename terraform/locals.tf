63f2b954b959179b3c2c8f9dd26822e8e3d0a5d7
Gonçalo Heleno <goncalo.heleno@camptocamp.com>
2024-09-30 14:57:54 +0200

    chore: put the cluster in hibernation until the presentation

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -22,56 +22,56 @@ locals {
         last_name  = "Heleno"
       }
       apps = {
-        auto_sync = true
-        list = [
-          {
-            name            = "text-generation-inference"
-            target_revision = "main"
-            chart_repo_url  = "https://github.com/camptocamp/self-hosted-llm-models-charts.git"
-            values_repo_url = "https://github.com/lentidas/self-hosted-llm-models-values.git"
-          },
-          {
-            name            = "chat-ui"
-            target_revision = "main"
-            chart_repo_url  = "https://github.com/camptocamp/self-hosted-llm-models-charts.git"
-            values_repo_url = "https://github.com/lentidas/self-hosted-llm-models-values.git"
-          },
-          {
-            name            = "llama-cpp"
-            target_revision = "main"
-            chart_repo_url  = "https://github.com/camptocamp/self-hosted-llm-models-charts.git"
-            values_repo_url = "https://github.com/lentidas/self-hosted-llm-models-values.git"
-          }
-        ]
+        # auto_sync = true
+        # list = [
+        #   {
+        #     name            = "text-generation-inference"
+        #     target_revision = "main"
+        #     chart_repo_url  = "https://github.com/camptocamp/self-hosted-llm-models-charts.git"
+        #     values_repo_url = "https://github.com/lentidas/self-hosted-llm-models-values.git"
+        #   },
+        #   {
+        #     name            = "chat-ui"
+        #     target_revision = "main"
+        #     chart_repo_url  = "https://github.com/camptocamp/self-hosted-llm-models-charts.git"
+        #     values_repo_url = "https://github.com/lentidas/self-hosted-llm-models-values.git"
+        #   },
+        #   # {
+        #   #   name            = "llama-cpp"
+        #   #   target_revision = "main"
+        #   #   chart_repo_url  = "https://github.com/camptocamp/self-hosted-llm-models-charts.git"
+        #   #   values_repo_url = "https://github.com/lentidas/self-hosted-llm-models-values.git"
+        #   # }
+        # ]
       }
     }
-    # bquartier = {
-    #   oidc = {
-    #     username   = "bquartier"
-    #     email      = "benoit.quartier@camptocamp.com"
-    #     first_name = "Benoît"
-    #     last_name  = "Quartier"
-    #   }
-    #   apps = {}
-    # }
-    # chornberger = {
-    #   oidc = {
-    #     username   = "chornberger"
-    #     email      = "christopher.hornberger@camptocamp.com"
-    #     first_name = "Christopher"
-    #     last_name  = "Hornberger"
-    #   }
-    #   apps = {
-    #     auto_sync = true
-    #     list = [
-    #       {
-    #         name            = "llama-cpp"
-    #         target_revision = "main"
-    #         chart_repo_url  = "https://github.com/camptocamp/self-hosted-llm-models-charts.git"
-    #         values_repo_url = "https://github.com/chornberger-c2c/self-hosted-llm-models-values.git"
-    #       },
-    #     ]
-    #   }
-    # }
+    bquartier = {
+      oidc = {
+        username   = "bquartier"
+        email      = "benoit.quartier@camptocamp.com"
+        first_name = "Benoît"
+        last_name  = "Quartier"
+      }
+      apps = {}
+    }
+    chornberger = {
+      oidc = {
+        username   = "chornberger"
+        email      = "christopher.hornberger@camptocamp.com"
+        first_name = "Christopher"
+        last_name  = "Hornberger"
+      }
+      apps = {
+        # auto_sync = true
+        # list = [
+        #   {
+        #     name            = "llama-cpp"
+        #     target_revision = "main"
+        #     chart_repo_url  = "https://github.com/camptocamp/self-hosted-llm-models-charts.git"
+        #     values_repo_url = "https://github.com/chornberger-c2c/self-hosted-llm-models-values.git"
+        #   },
+        # ]
+      }
+    }
   }
 }
